32eab6c375db65e17ca58a013712e64edb2179ac
amandashlim <amandashlim1@gmail.com>
2021-11-08 22:52:19 +0000

    Add Scrum diagram photo

--- data_exploration.py
+++ data_exploration.py
@@ -1,5 +1,8 @@
 # Write code that explores your data set
+# Import dataset
 import pandas as pd
-
-trees = pd.read_csv (r'Users/amandashlim/Downloads/cw1/Borough_tree_list_2021July.csv')
+trees = pd.read_csv (r'/Users/amandashlim/Downloads/cw1/Borough_tree_list_2021July.csv')
 print (trees)
+
+# Explore values and value counts in pandas
+trees.info()
