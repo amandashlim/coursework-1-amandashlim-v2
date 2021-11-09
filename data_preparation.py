70d63918b2243af203ae96c813003cc262cfd8de
amandashlim <amandashlim1@gmail.com>
2021-11-09 21:18:06 +0000

    Add dataset to repo in excel format and add data preporation code

--- data_preparation.py
+++ data_preparation.py
@@ -1,1 +1,18 @@
-# Write code that prepares your data
+import pandas as pd
+
+# Load excel file into pandas dataframe
+# Data from https://data.london.gov.uk/dataset/local-authority-maintained-trees
+trees = pd.read_excel("Borough_tree_list_2021July.xlsx")
+
+# Investigate data attributes
+# Rows and columns
+print(trees.shape)
+print(trees.head())
+print(trees.columns)
+print(trees.info(verbose=True))
+
+# Remove unecessary columns
+
+# Fill in missing values where possible
+
+# Omit rows with too little data
