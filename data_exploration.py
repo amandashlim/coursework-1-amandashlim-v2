37a9acfad72a912815fcff8fefad2fa83762e75d
amandashlim <amandashlim1@gmail.com>
2021-11-10 03:19:50 +0000

    Add pie by species, map by age, and bar by age graphs

--- data_exploration.py
+++ data_exploration.py
@@ -1,5 +1,4 @@
 # Import cleaned dataset
-import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
 import seaborn as sb
@@ -25,10 +24,38 @@ plt.title("Map of London Trees by Species")
 plt.legend(markerscale=100, fontsize=3, title="Species of Tree")
 plt.show()
 
-# BAR GRAPH BY SPECIES COUNT
+# PIE GRAPH BY SPECIES COUNT
+species_series = trees['tree_name'].value_counts(normalize=True)
+species_percentages = pd.DataFrame({'tree_name':species_series.index, 'percentage':species_series.values})
+
+# Aggregate percentages for all the species that have a percentage of less than 0.02
+# category into one "Other Species" category
+other_agg_percent = species_percentages[species_percentages['percentage'] < 0.02].sum(axis=0).iloc[1]
+
+# Remove individual rows for species with percentage less than 0.02 from the dataframe
+species_percentages = species_percentages[species_percentages['percentage'] >= 0.02]
+
+# Append Other Species row to end of dataframe
+species_percentages.loc[len(species_percentages)] = ["Various Species", other_agg_percent]
+
+# Plot pie chart
+species_pie = plt.pie(species_percentages['percentage'], labels=species_percentages['tree_name'], autopct='%1.1f%%')
+plt.title("Percentages of Tree Species in London Boroughs")
+plt.show()
 
 # MAP BY AGE
+age_map = sb.FacetGrid(data=trees, hue='age_group')
+age_map.map(plt.scatter, 'longitude', 'latitude', s=0.0005)
+plt.title("Map of London Borough Trees by Age Group")
+plt.legend(markerscale=300, title="Age of Tree")
+plt.show()
 
 # BAR GRAPH BY AGE
+age_bar = trees.age_group.value_counts().plot(kind="barh")
+plt.title("Count of Trees in London Boroughs by Age Group")
+plt.xlabel("Number of Trees")
+plt.show()
+
+# TREES PLANTED OVER TIME
 
 # TREES DUE FOR PRUNING THIS YEAR
